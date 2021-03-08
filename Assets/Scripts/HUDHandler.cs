55539fffb955199a5ea36695f2f18ad5727cf45e
Kenji <56013517+Ad1Ken@users.noreply.github.com>
2021-03-08 21:27:56 +0530

    Slot Machine

---
+++ Assets/Scripts/HUDHandler.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HUDHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject MainPanel;
+    [SerializeField] private GameObject MenuPanel;
+    [SerializeField] private GameObject GameOverPanel;
+
+    private void Start()
+    {
+        ActiveGameState(HUDstate.Menu);
+    }
+
+    private void ActiveGameState(HUDstate state)
+    {
+        if (state == HUDstate.Menu)
+        {
+            MenuPanel.SetActive(true);
+            MainPanel.SetActive(false);
+            GameOverPanel.SetActive(false);
+
+        }
+        else if (state == HUDstate.Game)
+        {
+            MainPanel.SetActive(true);
+            MenuPanel.SetActive(false);
+        }
+        else
+            GameOverPanel.SetActive(true);
+    }
+
+
+    public void OnClickPlay()
+    {
+        ActiveGameState(HUDstate.Game);
+    }
+
+    public void OnClickExit()
+    {
+        Application.Quit();
+    }
+
+    public void OnClickReturn()
+    {
+        ActiveGameState(HUDstate.Menu);
+        Clean();
+    }
+
+    
+    public void Clean()
+    {
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene("Game");
+        
+    }
+
+}
---
+++ Assets/Scripts/SlotHandler.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SlotHandler : MonoBehaviour
+{
+
+    [SerializeField] private List<Text> txtFields;
+    [SerializeField] private Text scoreText;
+    [SerializeField] private Text scoreMsg;
+    public GameObject endDisplay;
+    public GameObject gameDisplay;
+    public Button start;
+    public int get;
+
+    private bool startClicked;
+
+    private int scoreCount = 0;
+
+    public void Update()
+    {
+        if (startClicked)
+        {
+            for (int txts = 0; txts < txtFields.Count; txts++)
+            {
+                var randNum = UnityEngine.Random.Range(0, 10);
+                txtFields[txts].text = randNum.ToString();
+                if (txts == 2)
+                {
+                    get = randNum;
+                }
+            }
+        }
+    }
+
+    public void startOnClick()
+    {
+        startClicked = true;
+    }
+
+    public void StopOnClick()
+    {
+        startClicked = false;
+        if (get == 0 || get == 1)
+        {
+            StartCoroutine(ShowMessage("Bad Luck", 2));
+            start.interactable = false;
+            Invoke("EndDisplay", 1);
+        }
+        if (get == 6)
+        {
+            StartCoroutine(TimeoutEndTurnButton());
+            StartCoroutine(ShowMessage("Woahhh Its a Six!!!!!", 2));
+            AddScore(get);
+        }
+        if (get == 4)
+        {
+            StartCoroutine(TimeoutEndTurnButton());
+            StartCoroutine(ShowMessage("Woahhh Its a Four!!!!!", 2));
+            AddScore(get);
+        }
+        if (get == 2 || get == 3)
+        {
+            StartCoroutine(TimeoutEndTurnButton());
+            StartCoroutine(ShowMessage("Not Bad", 2));
+            AddScore(get);
+        }
+        if (get == 5)
+        {
+            StartCoroutine(TimeoutEndTurnButton());
+            StartCoroutine(ShowMessage("Awesome!", 2));
+            AddScore(get);
+        }
+        if (get == 7)
+        {
+            StartCoroutine(TimeoutEndTurnButton());
+            StartCoroutine(ShowMessage("Monster", 2));
+            AddScore(get);
+        }
+        if (get == 8)
+        {
+            StartCoroutine(TimeoutEndTurnButton());
+            StartCoroutine(ShowMessage("Aree bhai bhai bhai", 2));
+            AddScore(get);
+        }
+        if (get == 9)
+        {
+            StartCoroutine(TimeoutEndTurnButton());
+            StartCoroutine(ShowMessage("GodLike", 2));
+            AddScore(get);
+        }
+    }
+
+    private void AddScore(int val)
+    {
+        scoreCount += val;
+        scoreText.text = "Score : " + scoreCount.ToString();
+    }
+
+    public void EndDisplay()
+    {
+        gameDisplay.SetActive(false);
+        endDisplay.SetActive(true);
+    }
+
+    public void OnClick()
+    {
+        SceneManager.LoadScene("MenuScreen");
+    }
+
+    IEnumerator ShowMessage(string message, float delay)
+    {
+        scoreMsg.text = message;
+        scoreMsg.enabled = true;
+        yield return new WaitForSeconds(delay);
+        scoreMsg.enabled = false;
+    }
+
+    IEnumerator TimeoutEndTurnButton()
+    {
+        start.interactable = false;
+        yield return new WaitForSeconds(1.5f);
+        start.interactable = true;
+    }
+}
+
